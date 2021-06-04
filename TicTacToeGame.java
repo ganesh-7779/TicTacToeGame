fd09ffc90251d6d9e98df2b9c1b50246e44338b6
Ganesh <ganeshgavhad9@gmail.com>
2021-06-04 10:00:45 +0530

    UC1

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,8 +4,19 @@ public class TicTacToeGame {
 		public static char[] board = new char [10];
 
 		public static void main (String []args) {
-		
+			
+		boardOfGame();
 		}
+		public static void boardOfGame() {
+			
+			for (int index=1; index<board.length; index++) {
+				board[index]=' ';
+			}
+			
+		}
+		
+		
+		
 		}
 		
 		
